bb580d233e5b4844b6a0f44e6de4fb65a151fe93
solssak <gswoong1012@gmail.com>
2023-04-10 19:49:43 +0900

    feat: async request Handler 적용

--- routes/posts.js
+++ routes/posts.js
@@ -1,78 +1,73 @@
 const { Router } = require("express");
 const { Post } = require("../models");
+const asyncHandler = require("../utils/async-handler");
 
 const router = Router();
 
-router.get("/", async (req, res, next) => {
-  if (req.query.write) {
-    res.render("post/edit");
-    return;
-  }
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    if (req.query.write) {
+      res.render("post/edit");
+      return;
+    }
 
-  const posts = await Post.find({});
+    const posts = await Post.find({});
 
-  res.render("post/list", { posts });
-});
+    res.render("post/list", { posts });
+  })
+);
 
-router.get("/:shortId", async (req, res, next) => {
-  const { shortId } = req.params;
-  const post = await Post.findOne({
-    shortId,
-  });
+router.get(
+  "/:shortId",
+  asyncHandler(async (req, res) => {
+    const { shortId } = req.params;
+    const post = await Post.findOne({ shortId });
 
-  if (req.query.edit) {
-    res.render("post/edit", { post });
-    return;
-  }
+    if (req.query.edit) {
+      res.render("post/edit", { post });
+      return;
+    }
 
-  res.render("post/view", { post });
-});
+    res.render("post/view", { post });
+  })
+);
 
-router.post("/", async (req, res, next) => {
-  const { title, content } = req.body;
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { title, content } = req.body;
 
-  try {
     if (!title || !content) {
       throw new Error("제목과 내용을 입력해 주세요");
     }
 
-    const post = await Post.create({
-      title,
-      content,
-    });
+    const post = await Post.create({ title, content });
     res.redirect(`/posts/${post.shortId}`);
-  } catch (err) {
-    next(err);
-  }
-});
+  })
+);
 
-router.post("/:shortId", async (req, res, next) => {
-  const { shortId } = req.params;
-  const { title, content } = req.body;
-
-  try {
+router.post(
+  "/:shortId",
+  asyncHandler(async (req, res) => {
+    const { shortId } = req.params;
+    const { title, content } = req.body;
     if (!title || !content) {
       throw new Error("제목과 내용을 입력해 주세요");
     }
 
-    // shortId 로 게시글 수정
-    await Post.updateOne(
-      { shortId },
-      {
-        title,
-        content,
-      }
-    );
+    await Post.updateOne({ shortId }, { title, content });
     res.redirect(`/posts/${shortId}`);
-  } catch (err) {
-    next(err);
-  }
-});
+  })
+);
 
-router.delete("/:shortId", async (req, res, next) => {
-  const { shortId } = req.params;
-  await Post.deleteOne({ shortId });
-  res.send("OK");
-});
+router.delete(
+  "/:shortId",
+  asyncHandler(async (req, res, next) => {
+    const { shortId } = req.params;
+    await Post.deleteOne({ shortId });
+    res.send("OK");
+  })
+);
 
 module.exports = router;
